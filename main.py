ae9350f17eaba724d7b7b195b199c4dc15d2dc32
Nischay Bharadwaj <nischay.bharadwaj@gmail.com>
2021-04-03 13:06:58 +0530

    Added required files with a GUI window.

---
+++ main.py
@@ -0,0 +1,21 @@
+'''
+GUI Guess the number V1
+Nischay Bharadwaj
+03-04-2021
+'''
+
+import json
+from random import randint
+from tkinter import *
+from tkinter import messagebox
+
+class GTNv1:
+    def __init__(self):
+        self.root = Tk()
+
+    def rootActivate(self):
+        self.root.mainloop()
+
+
+gtnv1 = GTNv1()
+gtnv1.rootActivate()
