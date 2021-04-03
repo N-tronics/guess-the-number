73d7b2babaeb959ac55a15cea156f93328cadc95
Nischay Bharadwaj <nischay.bharadwaj@gmail.com>
2021-04-03 14:11:18 +0530

    v1.0.1: Added 3rd Row and getHs() method

--- main.py
+++ main.py
@@ -19,17 +19,21 @@ class GTNv1:
 
     def gui(self):
         self.name = Entry(self.root, relief=RIDGE, borderwidth=5, width=42, bg='grey', fg='white')
+        self.usrIn = Entry(self.root, relief=RIDGE, borderwidth=5, width=25, bg='grey', fg='white')
 
         nameLbl = Label(self.root, text='Enter your name:')
         diffLbl = Label(self.root, text='Difficulty:', anchor='e', width=14)
 
+
         easyBtn = Button(self.root, text='Easy(0~10)', state=NORMAL)
         mediumBtn = Button(self.root, text='Medium(0~100)', state=NORMAL)
         hardBtn = Button(self.root, text='Hard(0~500)', state=NORMAL)
+        chkBtn = Button(self.root, text='Check', width=27)
 
 #       ===============================================================================================================
 
         self.name.grid(row=0, column=1, columnspan=3)
+        self.usrIn.grid(row=2, column=0, columnspan=2)
 
         nameLbl.grid(row=0, column=0)
         diffLbl.grid(row=1, column=0)
@@ -37,6 +41,13 @@ class GTNv1:
         easyBtn.grid(row=1, column=1)
         mediumBtn.grid(row=1, column=2)
         hardBtn.grid(row=1, column=3)
+        chkBtn.grid(row=2, column=2, columnspan=2)
+
+    def getHs(self):
+        with open('high-scores.json') as f:
+            hs = json.load(f)
+
+        return hs
 
     def rootActivate(self):
         self.root.mainloop()
